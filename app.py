fd968f0bce41b98f6c60ad240e35fae2e05de6bd
Bhargav Sai <bhargavasai2468@example.com>
2026-05-28 10:34:59 +0530

    Fix port for Render

--- app.py
+++ app.py
@@ -213,4 +213,6 @@ def reset():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
